84ae58293b08a1827297c58da454db749bc3d060
nivratig <s3937114@student.rmit.edu.au>
2024-01-21 20:26:17 +1100

    feat: added edit profile form

--- Mcba/Controllers/ProfileController.cs
+++ Mcba/Controllers/ProfileController.cs
@@ -1,13 +1,18 @@
 using Mcba.Data;
 using Mcba.Middlewares;
+using Mcba.Models;
+using Mcba.Services;
+using Mcba.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Mcba.Controllers;
 
 [LoggedIn]
-public class ProfileController(McbaContext context) : Controller
+public class ProfileController(McbaContext context, IProfileService profileService) : Controller
 {
     private readonly McbaContext _dbContext = context;
+    private readonly IProfileService _profileService = profileService;
 
 
     public IActionResult Index()
@@ -15,5 +20,21 @@ public class ProfileController(McbaContext context) : Controller
         var customerID = HttpContext.Session.GetInt32("Customer");
         var customer = _dbContext.Customers.FirstOrDefault(b => b.CustomerID == customerID);
         return View(customer);
+    }
+
+    [HttpGet]
+    public IActionResult Edit() {
+        var customerID = HttpContext.Session.GetInt32("Customer");
+        var customer = _dbContext.Customers.FirstOrDefault(b => b.CustomerID == customerID);
+        return View(customer);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Customer customer) {
+        if (!ModelState.IsValid) {
+            return View();
+        }
+        // TODO: Redirectt to confirmation view
+        return RedirectToAction(nameof(Index));
     }
 }
