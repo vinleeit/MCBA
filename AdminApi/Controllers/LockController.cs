7521c72f721c3cf9f047d83303c446f9d06ab75e
Ravel Tanjaya <s3937118@student.rmit.edu.au>
2024-01-23 16:10:56 +1100

    feat: add account lock controller

--- AdminApi/Controllers/LockController.cs
+++ AdminApi/Controllers/LockController.cs
@@ -1,20 +1,31 @@
+using AdminApi.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AdminApi.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class LockController : ControllerBase
+public class LockController(IAdminRepo adminRepo) : ControllerBase
 {
+    private readonly IAdminRepo _adminRepo = adminRepo;
+
     [HttpPut("lock/{customerID}")]
     public IActionResult Lock(int customerID)
     {
-        return Ok();
+        return _adminRepo.GetCustomer(customerID) == null
+            ? NotFound()
+            : !_adminRepo.EditCustomerLock(customerID, true)
+                ? StatusCode(500)
+                : (IActionResult)Ok();
     }
 
     [HttpPut("unlock/{customerID}")]
     public IActionResult UnLock(int customerID)
     {
-        return Ok();
+        return _adminRepo.GetCustomer(customerID) == null
+            ? NotFound()
+            : !_adminRepo.EditCustomerLock(customerID, false)
+                ? StatusCode(500)
+                : (IActionResult)Ok();
     }
 }
