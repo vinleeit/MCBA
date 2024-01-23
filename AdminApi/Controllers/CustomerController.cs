fdce281a08810b05358eacb37b16de847820dc5d
Ravel Tanjaya <s3937118@student.rmit.edu.au>
2024-01-23 16:05:22 +1100

    feat: add customer admin api

--- AdminApi/Controllers/CustomerController.cs
+++ AdminApi/Controllers/CustomerController.cs
@@ -1,27 +1,41 @@
-using McbaData.Models;
+using AdminApi.Data;
+using McbaData.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AdminApi.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class CustomerController : ControllerBase
+public class CustomerController(IAdminRepo adminRepo) : ControllerBase
 {
+    private readonly IAdminRepo _adminRepo = adminRepo;
+
     [HttpGet]
-    public IActionResult GetAll()
+    public List<CustomerDto> GetAll()
     {
-        return Ok();
+        return _adminRepo.GetCustomers();
     }
 
     [HttpGet("{id}")]
     public IActionResult GetById(int id)
     {
-        return Ok();
+        var customer = _adminRepo.GetCustomer(id);
+        return customer == null ? NotFound() : Ok(customer);
+    }
+
+    [HttpGet("exists/{id}")]
+    public IActionResult GetAccountExists(int id)
+    {
+        return _adminRepo.GetCustomer(id) != null ? Ok() : NotFound();
     }
 
     [HttpPost("{id}")]
-    public Customer EditCustomer(int id, [FromBody] Customer customer)
+    public IActionResult EditCustomer(int id, [FromBody] CustomerEditDto customer)
     {
-        return customer;
+        return _adminRepo.GetCustomer(id) == null
+            ? NotFound()
+            : _adminRepo.EditCustomer(customer, id)
+                ? Ok()
+                : (IActionResult)StatusCode(500);
     }
 }
