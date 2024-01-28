d3fc9793a1ff942a34d672cd78bd6baa72a8d609
Ravel Tanjaya <s3937118@student.rmit.edu.au>
2024-01-28 16:40:24 +1100

    feat: completed admin panel

--- McbaAdmin/Models/LoginViewModel.cs
+++ McbaAdmin/Models/LoginViewModel.cs
@@ -1,12 +1,15 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace McbaAdmin.Models;
 public class LoginViewModel
 {
     [Required(ErrorMessage = "Username must not be empty")]
+    [JsonPropertyName("username")]
     public string? Username { get; set; }
 
     [Required(ErrorMessage = "Password must not be empty")]
     [DataType(DataType.Password)]
+    [JsonPropertyName("password")]
     public string? Password { get; set; }
 }
