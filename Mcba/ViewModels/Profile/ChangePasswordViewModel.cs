987dc392a9040badbc2fdefe9bfc5844ea877b0e
nivratig <s3937114@student.rmit.edu.au>
2024-01-28 15:17:15 +1100

    fix: fixed profile form label and validation error message

--- Mcba/ViewModels/Profile/ChangePasswordViewModel.cs
+++ Mcba/ViewModels/Profile/ChangePasswordViewModel.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 public class ChangePasswordViewModel
 {
-    [Required]
+    [Required(ErrorMessage = "Please insert a new password")]
     [DataType(DataType.Password)]
     public required string Password { get; set; }
 }
