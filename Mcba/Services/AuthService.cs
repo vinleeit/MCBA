6e10253eddb7d270e5ab8514dd52f185c1e0ecb9
Ravel Tanjaya <s3937118@student.rmit.edu.au>
2024-01-29 11:44:37 +1100

    hotfix: no error message on unknown id login

--- Mcba/Services/AuthService.cs
+++ Mcba/Services/AuthService.cs
@@ -13,21 +13,29 @@ public class AuthService(McbaContext context) : IAuthService
 
     public async Task<(AuthError? Error, int? Customer)> Login(string loginId, string password)
     {
-        var result = await (
-            from t in _dbContext.Logins
-            where t.LoginID == loginId
-            select new
-            {
-                Hash = t.PasswordHash,
-                Locked = t.Locked,
-            }
-        ).FirstAsync();
-        return (result.Locked)
-            ? (AuthError.Locked, null)
-            : (result.Hash == null)
-                ? (AuthError.InvalidCredential, null)
-                : new SimpleHash().Verify(password, result.Hash!)
-                    ? (null, await (from c in _dbContext.Customers where c.Login.LoginID == loginId select c.CustomerID).FirstOrDefaultAsync())
-                    : (AuthError.InvalidCredential, null);
+        try
+        {
+
+            var result = await (
+                from t in _dbContext.Logins
+                where t.LoginID == loginId
+                select new
+                {
+                    Hash = t.PasswordHash,
+                    Locked = t.Locked,
+                }
+            ).FirstAsync();
+            return (result.Locked)
+                ? (AuthError.Locked, null)
+                : (result.Hash == null)
+                    ? (AuthError.InvalidCredential, null)
+                    : new SimpleHash().Verify(password, result.Hash!)
+                        ? (null, await (from c in _dbContext.Customers where c.Login.LoginID == loginId select c.CustomerID).FirstOrDefaultAsync())
+                        : (AuthError.InvalidCredential, null);
+        }
+        catch (Exception)
+        {
+            return (AuthError.InvalidCredential, null);
+        }
     }
 }
